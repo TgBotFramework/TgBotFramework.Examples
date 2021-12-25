2c41d289f82e0b8b01da6b37e769534d4b070a29
Fedorus <alex.fedorus@gmail.com>
2021-12-25 02:09:05 +0200

    Added debuger handler

--- TelegramApiDebug/Program.cs
+++ TelegramApiDebug/Program.cs
@@ -10,6 +10,15 @@ await Host.CreateDefaultBuilder().ConfigureServices(services =>
     services.AddBotService<UpdateContext>("<token>", builder => builder
         .UseLongPolling(ParallelMode.SingleThreaded, new LongPollingOptions() { DebugOutput = true })
         .SetPipeline(pipeBuilder => pipeBuilder
+            .Use<DebugHandler>()
         )
     );
 }).RunConsoleAsync();
+
+public class DebugHandler : IUpdateHandler<UpdateContext>
+{
+    public async Task HandleAsync(UpdateContext context, UpdateDelegate<UpdateContext> next, CancellationToken cancellationToken)
+    {
+        // write anything and put break point. Then compare console output of update with what you see in debugger in context.Update
+    }
+}
